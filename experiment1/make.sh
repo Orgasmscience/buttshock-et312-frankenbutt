d014c3a7b65e7a5b75baa1f9e41daa365ddfba30
Fred <kinkyhacks@users.noreply.github.com>
2016-05-07 09:05:04 +0100

    Now we're in a different tree, just fixup the relatives

--- experiment1/make.sh
+++ experiment1/make.sh
@@ -8,7 +8,7 @@ avr-ld -mavr5 -o hookmain.elf hookmain.o
 avr-objcopy -Obinary hookmain.elf hookmain.bin
 
 # grab the decrypted firmware
-dd if=../firmware/312-16-decrypted.bin of=frankenbutt.bin bs=12288 count=1
+dd if=../../buttshock-et312-firmware/firmware/312-16-decrypted.bin of=frankenbutt.bin bs=12288 count=1
 
 # and add our stuff to location 0x3000
 dd if=hookmain.bin of=frankenbutt.bin skip=12288 seek=12288 bs=1
@@ -22,7 +22,5 @@ xxd -ps -c256 frankenbutt.bin | sed -f sed.txt | xxd -ps -r > frankenbutt2.bin
 mv frankenbutt2.bin frankenbutt.bin
 
 # add checksum and convert ready to upload
-../scripts/fw-utils.py -i frankenbutt.bin -e -o frankenbutt.upg
+../../buttshock-et312-firmware/scripts/fw-utils.py -i frankenbutt.bin -e -o frankenbutt.upg
 
-# hack
-cp frankenbutt.upg /tmp
